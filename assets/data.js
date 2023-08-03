73c6fd40c8132cea662a5dfcc4a7a0b0fa143301
Joseph <fallyfox@gmail.com>
2023-08-03 17:08:34 +0100

    flatlist lesson

---
+++ assets/data.js
@@ -0,0 +1,7 @@
+export const demoData = [
+    {id:1,product:'OX Industrial fan',amount:45000},
+    {id:2,product:'Dell laptop',amount:405000},
+    {id:3,product:'Blast plastic chair',amount:17000},
+    {id:4,product:'LG 2HP AC',amount:215000},
+    {id:5,product:'Izuzu pick up truck',amount:788000},
+]
--- screens/Scrolling.js
+++ screens/Scrolling.js
@@ -1,8 +1,44 @@
-import { View,Text,StyleSheet } from "react-native";
+import { View,Text,StyleSheet,ScrollView,FlatList } from "react-native";
+import { demoData } from "../assets/data";
 
 export function Scrolling() {
     return (
-        <></>
+        <View>
+            <ScrollView
+            showsHorizontalScrollIndicator={false}
+            horizontal={true}>
+                <View style={styles.box}></View>
+                <View style={styles.box}></View>
+                <View style={styles.box}></View>
+                <View style={styles.box}></View>
+                <View style={styles.box}></View>
+            </ScrollView>
+
+           <FlatList
+           data={demoData}
+           renderItem={({item}) => {
+            return (
+                <View style={styles.productBlock}>
+                    <Text>{item.product}</Text>
+                    <Text>{item.amount}</Text>
+                </View>
+            )
+           }}
+           key={({item}) => item.id}/>
+        </View>
     )
 }
 
+const styles = StyleSheet.create({
+    box:{
+        width:200,
+        height:200,
+        backgroundColor:'purple',
+        margin:20
+    },
+    productBlock:{
+        padding:16,
+        backgroundColor:'gray',
+        marginBottom:8
+    }
+})
